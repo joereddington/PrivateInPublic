3a1213ba49e97299dd450885f21c613ed13d6e3c
Joe Reddington <joe@joereddington.com>
2017-02-04 19:08:02 +0000

    Did some code quality stuff

--- javascript/privateinpublic.js
+++ javascript/privateinpublic.js
@@ -1,40 +1,29 @@
-
-function encrypt(input, passcode){
-passcode=passcode || "hardcoded"
-return CryptoJS.AES.encrypt(input,passcode).toString();
+function encrypt(input, passcode) {
+    passcode = passcode || "hardcoded";
+    return CryptoJS.AES.encrypt(input, passcode).toString();
 }
 
-function decrypt(ciphertext, passcode){
-passcode=passcode || "hardcoded"
-	var bytes  = CryptoJS.AES.decrypt(ciphertext, passcode);
-	var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-return plaintext
+function decrypt(ciphertext, passcode) {
+    passcode = passcode || "hardcoded";
+    var bytes = CryptoJS.AES.decrypt(ciphertext, passcode);
+    var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+    return plaintext;
 }
 
 
 
-function parse(input, passcode){
-passcode=passcode || "hardcoded"
-
-inputArray=input.split(/\[|\]/)
-console.log(inputArray)
-//for now we assume that we start with plaintext, there will be a test for this later. 
-//So every other element should be encrypted
-
-//let's use a for loop. 
-//
-
-for (var i=1; i<inputArray.length;i+=2){
-if(inputArray[i].startsWith("e:")){
-inputArray[i]="["+decrypt(inputArray[i].slice(2),passcode)+"]"
-}else{
-inputArray[i]="[e:"+encrypt(inputArray[i],passcode)+"]"
-}
-}
-
-console.log("Returning: "+inputArray.join(""))
-
-return inputArray.join("")
+function parse(input, passcode) {
+    passcode = passcode || "hardcoded";
+    inputArray = input.split(/\[|\]/);
+    console.log(inputArray);
+    for (var i = 1; i < inputArray.length; i += 2) {
+        if (inputArray[i].startsWith("e:")) {
+            inputArray[i] = "[" + decrypt(inputArray[i].slice(2), passcode) + "]";
+        } else {
+            inputArray[i] = "[e:" + encrypt(inputArray[i], passcode) + "]";
+        }
+    }
+    return inputArray.join("")
 
 
 }
--- tests/privateinpublic.test.js
+++ tests/privateinpublic.test.js
@@ -1,99 +1,96 @@
 /*
- * Unit tests for javascript/comscan.js
+ * Unit tests for javascript/privateinpublic.js
  *
  */
 describe('PublicInPrivate', function() {
-  
-  
-  // inject the HTML fixture for the tests
+
+
+    // inject the HTML fixture for the tests
 
     it('do we have a function called "encrypt"', function() {
         expect(encrypt).toBeDefined();
     });
 
 
-   it('Can the encrpyt function encrypt some text with a hardcoded passcode', function() {
-	var ciphertext=encrypt("Test String")
+    it('Can the encrpyt function encrypt some text with a hardcoded passcode', function() {
+        var ciphertext = encrypt("Test String")
 
-	// Decrypt from https://www.npmjs.com/package/crypto-js
-	var bytes  = CryptoJS.AES.decrypt(ciphertext, 'hardcoded');
-	var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+        // Decrypt from https://www.npmjs.com/package/crypto-js
+        var bytes = CryptoJS.AES.decrypt(ciphertext, 'hardcoded');
+        var plaintext = bytes.toString(CryptoJS.enc.Utf8);
         expect(plaintext).toBe("Test String");
     });
 
-   it('Can the encrpyt function encrypt some text with a passcode as argument', function() {
-	var ciphertext=encrypt("Test String","passcode")
+    it('Can the encrpyt function encrypt some text with a passcode as argument', function() {
+        var ciphertext = encrypt("Test String", "passcode")
 
-	// Decrypt from https://www.npmjs.com/package/crypto-js
-	var bytes  = CryptoJS.AES.decrypt(ciphertext, 'passcode');
-	var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+        // Decrypt from https://www.npmjs.com/package/crypto-js
+        var bytes = CryptoJS.AES.decrypt(ciphertext, 'passcode');
+        var plaintext = bytes.toString(CryptoJS.enc.Utf8);
         expect(plaintext).toBe("Test String");
     });
 
-   it('Checking that Spy.callFake does what I want...', function() {
-   spyOn(window, "encrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-	var ciphertext=encrypt("Test String","passcode")
+    it('Checking that Spy.callFake does what I want...', function() {
+        spyOn(window, "encrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        var ciphertext = encrypt("Test String", "passcode")
         expect(ciphertext).toBe("gnirtS tseT");
     });
 
 
-   it('can replace only the text surounded by a marker', function() {
-   spyOn(window, "encrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-	var ciphertext=parse("Test [This part of this] String","passcode")
+    it('can replace only the text surounded by a marker', function() {
+        spyOn(window, "encrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        var ciphertext = parse("Test [This part of this] String", "passcode")
         expect(ciphertext).toBe("Test [e:siht fo trap sihT] String");
     });
 
 
-  it('leaves strings with NO marker alone', function() {
-   spyOn(window, "encrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-	var ciphertext=parse("Test String","passcode")
+    it('leaves strings with NO marker alone', function() {
+        spyOn(window, "encrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        var ciphertext = parse("Test String", "passcode")
         expect(ciphertext).toBe("Test String");
     });
 
 
-  it('deals with strings that start with a secret', function() {
-   spyOn(window, "encrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-	var ciphertext=parse("[we have a ] Test String","passcode")
+    it('deals with strings that start with a secret', function() {
+        spyOn(window, "encrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        var ciphertext = parse("[we have a ] Test String", "passcode")
         expect(ciphertext).toBe("[e: a evah ew] Test String");
     });
 
 
 
-  it('correctly replaces several times in a string', function() {
-   spyOn(window, "encrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-	var ciphertext=parse("[we have a ] Test [an amazing test] String","passcode")
+    it('correctly replaces several times in a string', function() {
+        spyOn(window, "encrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        var ciphertext = parse("[we have a ] Test [an amazing test] String", "passcode")
         expect(ciphertext).toBe("[e: a evah ew] Test [e:tset gnizama na] String");
     });
 
 
-  it('correctly decrpt text', function() {
-   spyOn(window, "encrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-   spyOn(window, "decrypt").and.callFake(function(string, passcode) {
-      return string.split("").reverse().join("");//from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
-    });
-	var ciphertext=parse("[e: a evah ew] Test [e:tset gnizama na] String","passcode")
+    it('correctly decrpt text', function() {
+        spyOn(window, "encrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        spyOn(window, "decrypt").and.callFake(function(string, passcode) {
+            return string.split("").reverse().join(""); //from https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.z8u4jebnx
+        });
+        var ciphertext = parse("[e: a evah ew] Test [e:tset gnizama na] String", "passcode")
         expect(ciphertext).toBe("[we have a ] Test [an amazing test] String");
     });
 
-
-
-
-  it('correctly replaces several times in a string without spy', function() {
-	starttext="[we have a ] Test [an amazing test] String"
-	var ciphertext=parse(starttext,"hardcoded")
-	var plaintext=parse(ciphertext)
+    it('correctly replaces several times in a string without spy', function() {
+        starttext = "[we have a ] Test [an amazing test] String"
+        var ciphertext = parse(starttext, "hardcoded")
+        var plaintext = parse(ciphertext)
         expect(plaintext).toBe(starttext);
     });
 
