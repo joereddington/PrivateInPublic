9eb19cbb7eb2192f36b3258a7f50ea675f69d363
Joe Reddington <joe@joereddington.com>
2017-02-04 11:35:29 +0000

    First test passing

---
+++ javascript/publicinprivate.js
@@ -0,0 +1,6 @@
+
+
+function encrypt(input){
+
+return 0
+}
---
+++ tests/publicinprivate.test.js
@@ -0,0 +1,11 @@
+/*
+ * Unit tests for javascript/comscan.js
+ *
+ */
+describe('PublicInPrivate', function() {
+
+    it('do we have a function called "encrypt"', function() {
+        expect(encrypt).toBeDefined();
+    });
+
+});
